350faff5670e9a4fea61af8cd85cb059a61f340b
nitsaw09 <niteshghuge09@gmail.com>
2025-02-28 12:22:29 +0530

    error handling

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const mongoose = require('mongoose');
 const betRotes = require('./modules/bet/bet.route');
 const roundRoutes = require('./modules/round/round.route');
-const RoundModel = require('./models/round');
 const dbConnect = require('./config/dbConnect');
+const { errorResponse } = require('./utils/response');
+const HttpException = require('./utils/http/httpException');
+const httpCodes = require('./utils/http/httpCodes');
 
 dotenv.config();
 
@@ -23,7 +24,19 @@ app.use(express.json());
 // api routes
 app.use('/api/v1/bets', betRotes);
 app.use('/api/v1/rounds', roundRoutes);
-  
+
+// handling 404 error
+app.use((req, res, next) => {
+  const error = new HttpException(httpCodes.NOT_FOUND);
+  next(error);
+});
+
+// handling client & server error response
+app.use((error, req, res, next) => {
+  errorResponse(res, error);
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
